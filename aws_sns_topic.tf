be72e0718720258d7c6f6b0c384abdad7b8c4d1c
papiamento <1024836+papiamento@users.noreply.github.com>
2020-04-06 16:16:20 -0400

    add sns topic

--- aws_sns_topic.tf
+++ aws_sns_topic.tf
@@ -1,3 +1,10 @@
 resource "aws_sns_topic" "monitor-availability-topic" {
   name = "monitor-availability"
 }
+
+
+resource "aws_sns_topic_subscription" "sms" {
+  endpoint = var.phone-number
+  protocol = "sms"
+  topic_arn = aws_sns_topic.monitor-availability-topic.arn
+}
---
+++ inputs.tf
@@ -0,0 +1,3 @@
+variable "phone-number" {
+  type = string
+}
